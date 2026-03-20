19f0005c290bc0e63bc9c10da5da4866002ccdc5
andrew <andrew.borg@bizcuits.io>
2026-03-20 20:30:49 +0100

    feat(core): Work on CRM auth

--- packages/auth/src/modules/casl/types/option.type.ts
+++ packages/auth/src/modules/casl/types/option.type.ts
@@ -1,7 +1,10 @@
 import { Type } from '@nestjs/common';
 
 export type Option<T extends Type<any> = any> = {
+  /** The part of the request to search in */
   in: 'query' | 'body' | 'params' | 'headers';
+  /** The key to search for in the request to use in order to find an identifier */
   use: string;
+  /** The entity property to search in using the identifier as a value */
   findBy: keyof InstanceType<T>;
 };
