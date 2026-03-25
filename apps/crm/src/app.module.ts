f6fcf45e2ec2b5fb6eecaaec2662cdea943a29aa
andrew <andrew.borg@bizcuits.io>
2026-03-25 13:58:26 +0100

    feat(core): Add integrations CRUD

--- apps/crm/src/app.module.ts
+++ apps/crm/src/app.module.ts
@@ -5,22 +5,32 @@ import { SentryModule } from '@sentry/nestjs/setup';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Transport, ClientsModule } from '@nestjs/microservices';
 
+import { AuthModule } from '@crm/auth';
 import { SwaggerModule } from '@crm/swagger';
 import { DatabaseModule } from '@crm/database';
 import { ValidationModule } from '@crm/validation';
 
 import appConfig from './config/app/app.config';
+import authConfig from './config/auth/auth.config';
 import databaseConfig from './config/database/database.config';
 
 import { CommonModule } from './common/common.module';
 import { HealthModule } from './health/health.module';
 import { AppConfig } from './config/app/app-config.type';
+import { AuthConfig } from './config/auth/auth-config.type';
 import { IntegrationModule } from './integration/integration.module';
 import { DatabaseConfig } from './config/database/database-config.type';
 
 @Module({
   imports: [
     SentryModule.forRoot(),
+    AuthModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (c: ConfigService<{ auth: AuthConfig }>) => ({
+        jwtSecret: c.getOrThrow<string>('auth.secret', { infer: true }),
+        refreshSecret: c.getOrThrow<string>('auth.refreshSecret', { infer: true }),
+      }),
+    }),
     ClientsModule.registerAsync({
       isGlobal: true,
       clients: [
@@ -52,7 +62,7 @@ import { DatabaseConfig } from './config/database/database-config.type';
     }),
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig, databaseConfig],
+      load: [appConfig, authConfig, databaseConfig],
       envFilePath: ['.env'],
     }),
     CommonModule,
